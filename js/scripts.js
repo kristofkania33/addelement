6df66758f08240156dedd9cd6883931210800880
kristofkania33 <zyhkana25@wp.pl>
2017-06-02 13:59:00 +0200

    addelement

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem'),
+	number = document.getElementsByTagName('li');
+
+add.addEventListener('click', function() {
+var element = document.createElement('li');
+for(i=0;i<=number.length;i++) {
+    element[i].innerHTML = 'item ' + i;
+}
+list.appendChild(element);
+}
